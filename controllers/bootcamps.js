d4321920d6eb7a3357c9154c0d71cc6d0690eb98
Kirk Vander Kuy <kvk807@gmail.com>
2021-05-16 21:59:18 -0500

    39-added course model, routes and controllers, populate, virtuals and cascade delete

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -15,7 +15,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   // Copy req.query
   let reqQuery = { ...req.query };
   // Fields to exclude
-  const removeFields = ["select"];
+  const removeFields = ["select", "sort", "page", "limit"];
   // Loop over removeFields and delete them from reqQuery
   removeFields.forEach((param) => delete reqQuery[param]);
   // grab the query string from the request object
@@ -26,7 +26,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     (match) => `$${match}`
   );
   // create the query variable by turning the query string back into JSON
-  query = Bootcamp.find(JSON.parse(queryStr));
+  query = Bootcamp.find(JSON.parse(queryStr)).populate("courses");
   //////////////////////////////
   // Filtering functionality ///
   //////////////////////////////
@@ -42,14 +42,52 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     const sortBy = req.query.sort.split(",").join(" ");
     query = query.sort(sortBy);
   } else {
-    query = query.sort("-createdAt");
+    query = query.sort({ createdAt: "asc", name: -1 });
+  }
+  /////////////////////////////////////
+  //  Pagination functionality  ///////
+  /////////////////////////////////////
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments(JSON.parse(queryStr));
+
+  query = query.skip(startIndex).limit(limit);
+
+  /////////////////////////////////
+  // Execute our query  ///////////
+  /////////////////////////////////
+  const bootcamps = await query;
+
+  ////////////////////////////////////////
+  // Set previous and next page results //
+  ////////////////////////////////////////
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
   }
 
-  // run our query
-  const bootcamps = await query;
-  res
-    .status(200)
-    .json({ success: true, count: bootcamps.length, data: bootcamps });
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+
+  ////////////////////////////////////////
+  // Send query results to api endpoint //
+  ////////////////////////////////////////
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    pagination,
+    data: bootcamps,
+  });
 });
 
 // @desc    Get single bootcamp
@@ -99,13 +137,15 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Resurce not found with id of ${req.params.id}`, 404)
     );
   }
+
+  bootcamp.remove();
 
   res.status(200).json({ success: true, data: {} });
 });
